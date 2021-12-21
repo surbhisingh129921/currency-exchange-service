76b8059b2f920c721cdc33d2cee908cb53400403
surbhi <surbhi.singh.1295@gmail.com>
2021-12-21 12:03:27 +0530

    refactored table name

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into excahnge_value(id,from,to,conversion_multiple)
+insert into exchange_value(id,from,to,conversion_multiple)
 value(10001,'USD','INR',65);
-insert into excahnge_value(id,from,to,conversion_multiple)
+insert into exchange_value(id,from,to,conversion_multiple)
 value(10002,'EUR','INR',75);
-insert into excahnge_value(id,from,to,conversion_multiple)
+insert into exchange_value(id,from,to,conversion_multiple)
 value(10002,'AUD','INR',85);
