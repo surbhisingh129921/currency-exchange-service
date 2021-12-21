4714ca0ef94ed43a85c6bb5896a6d4c904053ac6
surbhi <surbhi.singh.1295@gmail.com>
2021-12-21 12:02:17 +0530

    added sql query to insert values in h2 db

--- src/main/java/com/microservices/currencyexchangeservice/ExchangeValue.java
+++ src/main/java/com/microservices/currencyexchangeservice/ExchangeValue.java
@@ -1,11 +1,17 @@
 package com.microservices.currencyexchangeservice;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class ExchangeValue {
-
+    @Id
     private long id;
+    @Column(name="currency_from")
     private String from;
+    @Column(name="currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private int port;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into excahnge_value(id,from,to,conversion_multiple)
+value(10001,'USD','INR',65);
+insert into excahnge_value(id,from,to,conversion_multiple)
+value(10002,'EUR','INR',75);
+insert into excahnge_value(id,from,to,conversion_multiple)
+value(10002,'AUD','INR',85);
